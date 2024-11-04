963bad4e3fcec01de1f97772b0cb1b63f7cb9248
LudmilaMiguens <miguensludmila@gmail.com>
2024-11-04 20:36:11 -0300

    mercado pago no anda

--- src/mercadopago/mercadopago.controller.ts
+++ src/mercadopago/mercadopago.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, HttpException, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import {MercadopagoService} from './mercadopago.service';
 
 @Controller('mercadopago')
@@ -12,18 +12,15 @@ async getPreference (){
 
 }
 
-@Post('/create-preference')
-async createPreference(@Body() preferenceData) {
+  @Post('/create-preference')
+  async createPreference(@Body() preferenceData) {
     try {
-        const response = await this.mercadoPagoService.createPreference(preferenceData);
-        return { 
-            id: response.body.id,
-            init_point: response.body.init_point // Asegúrate de incluir esto
-        };
-    } catch (error) {
-        console.error('Error al crear preferencia:', error);
-        throw new HttpException('No se pudo crear la preferencia', HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-}
+      const response = await this.mercadoPagoService.createPreference(preferenceData);
 
+      return { id: response.body.id };
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
 }
