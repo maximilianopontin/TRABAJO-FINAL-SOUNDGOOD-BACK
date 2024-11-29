1d4bb85bfbaf848881d11324a6e75d8590d2c506
maximilianopontin <maxipontin92@gmail.com>
2024-11-28 21:12:20 -0300

    comente throttler en app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,29 @@ import { PlaylistsModule } from './playlists/playlists.module';
 import { AutenticacionModule } from './autenticacion/autenticacion.module';
 import { MercadoPagoController } from './mercadopago/mercadopago.controller';
 import { MercadopagoService } from './mercadopago/mercadopago.service';
-import { FilesController } from './files/files.controller';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 import { FilesModule } from './files/files.modules';
+import { HealthModule } from './health/health.module';
+/*
+@Module({
+  imports: [ ThrottlerModule.forRoot([
+    {
+      //name podemos agregarle nombre con la cantidad de peticion que le permito por minuto, esto en caso de que necesitemos distintos limites para las solictudes
+      ttl: 60000,// ttl Tiempo de vida en segundos para la limitación de solicitudes (60 segundos)
+      limit: 10, // limit Número máximo de solicitudes permitidas por IP en el periodo de tiempo (10 solicitudes)
+    },
+  ]),
+    UsuariosModule, CancionesModule, ArtistasModule, GenerosModule, PlaylistsModule, AutenticacionModule, FilesModule,HealthModule],
+  controllers: [AppController, MercadoPagoController],
+    
+  providers: [AppService, MercadopagoService, {provide: APP_GUARD,
+    useClass: ThrottlerGuard
+  }],
+    
 
+})
+export class AppModule {}*/
 @Module({
   imports: [UsuariosModule, CancionesModule, ArtistasModule, GenerosModule, PlaylistsModule, AutenticacionModule, FilesModule],
   controllers: [AppController, MercadoPagoController],
